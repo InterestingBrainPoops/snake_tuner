5bcbd22ef3f96e8450bd682d60bc56fe362fa25b
Het Satasiya <satasiyahet@gmail.com>
2023-06-29 13:44:16 -0700

    finish net impl

--- src/net.rs
+++ src/net.rs
@@ -1,34 +1,32 @@
 use std::ops::Mul;
 
-use nalgebra::{SMatrix, SVector};
+use nalgebra::{DMatrix, DVector, SMatrix, SVector};
 
 use crate::activation::ActivationFunction;
 
-pub struct Net<const I: usize, const H: usize, const O: usize> {
-    pub weights_ih: SMatrix<f64, H, I>,
-    pub weights_ho: SMatrix<f64, O, H>,
-    pub bias_h: SMatrix<f64, H, 1>,
-    pub bias_o: SMatrix<f64, O, 1>,
-    pub lr: f64,
+pub struct Layer {
+    pub weights_io: DMatrix<f64>,
+    pub bias_o: DVector<f64>,
 }
 
-pub struct Layer<const I: usize, const O: usize> {
-    pub weights_io: SMatrix<f64, O, I>,
-    pub bias_o: SMatrix<f64, O, 1>,
-}
-
-impl<const I: usize, const O: usize> Layer<I, O> {
+impl Layer {
+    pub fn new(inputs: usize, outputs: usize) -> Layer {
+        Layer {
+            weights_io: DMatrix::zeros(outputs, inputs),
+            bias_o: DVector::zeros(outputs),
+        }
+    }
     pub fn backprop<A: ActivationFunction>(
         &mut self,
-        out_errors: SVector<f64, O>,
-        output: SVector<f64, O>,
-        input: SVector<f64, I>,
+        out_errors: DVector<f64>,
+        output: DVector<f64>,
+        input: DVector<f64>,
         learning_rate: f64,
-    ) -> SVector<f64, I> {
+    ) -> DVector<f64> {
         let gradients = output.map(|x| A::derivative(x)).component_mul(&out_errors) * learning_rate;
 
         let input_t = input.transpose();
-        let weights_io_deltas = gradients.mul(input_t);
+        let weights_io_deltas = gradients.clone().mul(input_t);
 
         self.weights_io += weights_io_deltas;
 
@@ -36,61 +34,51 @@ impl<const I: usize, const O: usize> Layer<I, O> {
         let wio_t = self.weights_io.transpose();
         wio_t.mul(out_errors)
     }
+
+    pub fn forward<A: ActivationFunction>(&self, input: DVector<f64>) -> DVector<f64> {
+        (self.weights_io.clone().mul(input) + self.bias_o.clone()).map(|x| A::evaluate(x))
+    }
 }
-impl<const I: usize, const H: usize, const O: usize> Net<I, H, O> {
-    pub fn forward<A: ActivationFunction>(&self, input: SVector<f64, I>) -> SVector<f64, O> {
-        let mut hidden = self.weights_ih.mul(input);
-        hidden += self.bias_h;
-        hidden = hidden.map(|x| A::evaluate(x));
 
-        let mut output = self.weights_ho.mul(hidden);
-        output += self.bias_o;
-        output = output.map(|x| A::evaluate(x));
+pub struct Net {
+    layers: Vec<Layer>,
+}
 
-        output
+impl Net {
+    pub fn new(layer_sizes: Vec<usize>) -> Net {
+        let mut layers = vec![];
+        for (idx, in_size) in layer_sizes[..(layer_sizes.len() - 1)].iter().enumerate() {
+            layers.push(Layer::new(*in_size, layer_sizes[idx + 1]));
+        }
+        Net { layers }
     }
 
-    pub fn train<A: ActivationFunction>(
+    pub fn forward<A: ActivationFunction>(&self, input: DVector<f64>) -> DVector<f64> {
+        let mut input = input;
+        for x in &self.layers {
+            input = x.forward::<A>(input.clone());
+        }
+        input
+    }
+
+    pub fn backward<A: ActivationFunction>(
         &mut self,
-        input: SVector<f64, I>,
-        target: SVector<f64, O>,
+        input: DVector<f64>,
+        expected: DVector<f64>,
+        learning_rate: f64,
     ) {
-        let mut hidden = self.weights_ih.mul(input);
-        hidden += self.bias_h;
-        hidden = hidden.map(|x| A::evaluate(x));
-
-        let mut output = self.weights_ho.mul(hidden);
-        output += self.bias_o;
-        output = output.map(|x| A::evaluate(x));
-
-        let output_error = target - output;
-        // begin H->O layer
-        let gradients = output
-            .map(|x| A::derivative(x))
-            .component_mul(&output_error)
-            * self.lr;
-
-        let hidden_t = hidden.transpose();
-        let weight_ho_deltas = gradients.mul(hidden_t);
-
-        self.weights_ho += weight_ho_deltas;
-
-        self.bias_o += gradients;
-        let who_t = self.weights_ho.transpose();
-        let hidden_errors = who_t.mul(output_error);
-
-        // end H->0 layer
-
-        let hidden_gradient = hidden
-            .map(|x| A::derivative(x))
-            .component_mul(&hidden_errors)
-            * self.lr;
-
-        let inputs_t = input.transpose();
-        let weights_ih_deltas = hidden_gradient.mul(inputs_t);
-
-        self.weights_ih += weights_ih_deltas;
-
-        self.bias_h += hidden_gradient;
+        let mut layer_intermmediates = vec![input];
+        for (idx, layer) in self.layers.iter().enumerate() {
+            layer_intermmediates.push(layer.forward::<A>(layer_intermmediates[idx].clone()));
+        }
+        let mut error = expected - layer_intermmediates.last().unwrap().clone();
+        for (idx, layer) in self.layers.iter_mut().enumerate().rev() {
+            error = layer.backprop::<A>(
+                error,
+                layer_intermmediates[idx + 1].clone(),
+                layer_intermmediates[idx].clone(),
+                learning_rate,
+            );
+        }
     }
 }
